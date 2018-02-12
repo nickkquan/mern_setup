081021bdddb9594978bfaa67e0160abf0d9de465
Nick Quan <nickkquan@gmail.com>
2018-02-12 12:52:42 -0800

    Initial client setup completed

---
+++ client/src/components/about.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class About extends Component {
+	render() {
+		return (
+			<div>
+				<h1>About Us</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, blanditiis similique quam ad qui fugit
+					saepe totam, nisi cumque officia commodi dolor cupiditate facilis vero ipsam temporibus perferendis!
+					Culpa, facere.
+				</p>
+			</div>
+		);
+	}
+}
+
+export default About;
---
+++ client/src/components/contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+	render() {
+		return (
+			<div>
+				<h1>How to reach us</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, blanditiis similique quam ad qui fugit
+					saepe totam, nisi cumque officia commodi dolor cupiditate facilis vero ipsam temporibus perferendis!
+					Culpa, facere.
+				</p>
+			</div>
+		);
+	}
+}
+
+export default Contact;
